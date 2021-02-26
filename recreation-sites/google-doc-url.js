195aa49446225432340f7a60ce7d1855fc5f58e2
James Taylor <35105264+jtatfranklin@users.noreply.github.com>
2021-02-26 16:16:31 -0600

    data sheet
    Link to new G-sheet of rec site data.

--- recreation-sites/google-doc-url.js
+++ recreation-sites/google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UEaBQMgny1xv5hyGBBPOv2TWc1-Q-Jdwfg1VqtSUDg0/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SrdM1eT4sNml-oq4_I6wodyFBhflymjUAmdSDIrjykY/edit#gid=1729564843';
